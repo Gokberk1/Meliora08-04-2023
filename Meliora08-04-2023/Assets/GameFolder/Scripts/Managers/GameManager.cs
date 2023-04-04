666f76cb1f087d0400a1dc6c24cb9461812d97e0
Gökberk <gokberktamer@gmail.com>
2023-04-04 16:43:21 +0300

    Singleton design pattern
    singleton design yazıldı ve game manager kodu oluşturuldu

---
+++ Meliora08-04-2023/Assets/GameFolder/Scripts/Managers/GameManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Meliora08_04_2023.Managers
+{
+    public class GameManager : MonoBehaviour
+    {
+
+    }
+}
+
